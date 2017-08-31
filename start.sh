5bd5ce951a1861f31946ee14c399d12964fb88f6
cousinea <cousinea@umich.edu>
2017-08-31 14:56:54 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # link apace configuration files from preloaded secret.
-ln -sf /tmp/apache-conf/httpd.conf /usr/local/apache2/conf/httpd.conf
+#ln -sf /tmp/apache-conf/httpd.conf /usr/local/apache2/conf/httpd.conf
 
 # Redirect logs to stdout and stderr for docker reasons.
 ln -sf /dev/stdout /usr/local/apache2/logs/access_log
