3fc5555aa3fecf3272999d2f200b796c95a8b753
cousinea <cousinea@umich.edu>
2017-11-14 13:12:15 -0500

    cleanup & switch to SSL-enabled

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM httpd:2.4
+FROM apache2-cosign:2.4.29
 
 EXPOSE 443
 
--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # link apace configuration files from preloaded secret.
-ln -sf /tmp/apache-conf/httpd.conf /usr/local/apache2/conf/httpd.conf
+ln -sf /tmp/httpd/httpd.conf /usr/local/apache2/conf/httpd.conf
+ln -sf /tmp/httpd/httpd-ssl.conf /usr/local/apache2/conf/extra/httpd-ssl.conf
 
 # Redirect logs to stdout and stderr for docker reasons.
 ln -sf /dev/stdout /usr/local/apache2/logs/access_log
